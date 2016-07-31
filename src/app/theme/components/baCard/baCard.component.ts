858ffa9782ada328f6f3d1fe7d88837bff64fdc4
Itai Gordon <itaig@il.ibm.com>
2016-07-31 15:39:32 +0300

    add title with products and snapshots

--- src/app/theme/components/baCard/baCard.component.ts
+++ src/app/theme/components/baCard/baCard.component.ts
@@ -1,10 +1,10 @@
 import {Component, ViewEncapsulation, ViewChild, Input} from '@angular/core';
 import {BaCardBlur} from './baCardBlur.directive';
-
+import { DROPDOWN_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 @Component({
   selector: 'ba-card',
   styles: [require('./baCard.scss')],
-  directives: [BaCardBlur],
+  directives: [BaCardBlur,DROPDOWN_DIRECTIVES],
   template: require('./baCard.html'),
   encapsulation: ViewEncapsulation.None
 })
